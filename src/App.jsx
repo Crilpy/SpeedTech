382510b8f84e303aaa7ba13b5fec00597b34067f
Crilpy <albertstoyanov2006@gmail.com>
2024-12-16 19:37:46 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,38 @@
-import { Routes, Route } from "react-router-dom"
-import AuthLayout from "./components/auth/layout"
+import { Routes, Route } from "react-router-dom";
+import AuthLayout from "./components/auth/layout";
+import AuthLogin from "./pages/auth/loging";
+import AuthRegister from "./pages/auth/register";
+import AdminLayout from "./components/admin-view/layout";
+import AdminDashboard from "./pages/admin-view/dashboard";
+import AdminProducts from "./pages/admin-view/products";
+import AdminOrders from "./pages/admin-view/orders";
+import AdminFeatures from "./pages/admin-view/features";
+import ShoppingLayout from "./components/shopping-view/layout";
+import NotFound from "./pages/not-found";
 
 function App() {
-  
-
   return (
     <div className="flex flex-col overflow-hidden bg-white">
       <h1>Header Component</h1>
 
       <Routes>
-        <Route path="/auth" element={<AuthLayout></AuthLayout>}>
-
+        <Route path="/auth" element={<AuthLayout />}>
+          {/* Correctly use JSX elements for the `element` prop */}
+          <Route path="loging" element={<AuthLogin />} />
+          <Route path="register" element={<AuthRegister />} />
         </Route>
+        <Route path="/admin" element={<AdminLayout/>}>
+          <Route path="dashboard" element={<AdminDashboard/>}/>
+          <Route path="products" element={<AdminProducts/>}/>
+          <Route path="orders" element={<AdminOrders/>}/>
+          <Route path="features" element={<AdminFeatures/>}/>
+        </Route>
+        <Route path="/shop" element={<ShoppingLayout/>}></Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
-       
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
